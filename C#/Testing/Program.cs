44a73b52fdfeeeea1927f2aae14d7cbe4333408e
JeremyXie123 <jeremy.xie@tdsb.on.ca>
2021-08-10 23:33:23 -0400

    Add markdown example

--- C#/Testing/Program.cs
+++ C#/Testing/Program.cs
@@ -3,21 +3,11 @@
 namespace Testing {
     class Program {
         static void Main(string[] args) {
-            // <type> <name> = <value>
-            int testInt = 1;
-            double testDouble = 3.14;
-            char testChar = 'A';
-            string testString = "Hi There.";
-            bool testBool = true;
+            int myInt = 9;
+            double myDouble = myInt;       // Automatic casting: int to double
 
-            var unknownVar = "Hello World!"; // var
-
-            Console.WriteLine($"testInt: {testInt}");
-            Console.WriteLine($"testDouble: {testDouble}");
-            Console.WriteLine($"testChar: {testChar}");
-            Console.WriteLine($"testString: {testString}");
-            Console.WriteLine($"testBool: {testBool}");
-            Console.WriteLine($"unknownVar: {unknownVar}");
+            Console.WriteLine(myInt);      // Outputs 9
+            Console.WriteLine(myDouble);   // Outputs 9
         }
     }
 }
